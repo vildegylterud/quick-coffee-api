3e9453380343dbd098d5371cdefe3761dd50f8cc
Vilde Gylterud <vildegylterud@outlook.com>
2023-10-18 12:39:42 +0200

    commit

--- quick-coffee-api/Features/Products/ProductController.cs
+++ quick-coffee-api/Features/Products/ProductController.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace quick_coffee_api.Controllers;
 
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("[controller]")]
 public class ProductController
 {
     
