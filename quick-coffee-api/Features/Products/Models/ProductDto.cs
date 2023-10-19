2bfd6b67470b8b1f54a417f252a6cb93dd47400e
Vilde Gylterud <vildegylterud@outlook.com>
2023-10-19 15:01:45 +0200

    made product dto class

--- quick-coffee-api/Features/Products/Models/ProductDto.cs
+++ quick-coffee-api/Features/Products/Models/ProductDto.cs
@@ -1,6 +1,16 @@
+using quick_coffee_api.Features.ExtraProducts;
+
 namespace quick_coffee_api.Models;
 
 public class ProductDto
 {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string ImgUrl { get; set; }
+    public string ProductType { get; set; }
     
+    public List<ExtraProductDocument> ExtraProduct { get; set; }
+        = new List<ExtraProductDocument>();
 }
